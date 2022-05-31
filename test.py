5aa2acdf3bfa8f6a2ca6168d161d90e1c89c6bd8
Robbysseus <87700333+Robbysseus@users.noreply.github.com>
2022-05-31 19:15:06 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,4 @@
-# Labeling empty cells with an informative value -----------------
-df_empty_filled = empty_fill(df)
-
-# Standardizing the species columns ------------------------------
-df_filled = standardize_sp(df_empty_filled)
-
-# Standardizing the sequence length ------------------------------
-df_full = keep_min_max_len(df_filled, min_max = (1000, 2000))
-
-# Removal of highly underrepresented species ---------------------
-df_full_min = min_entries(df_full, taxon = 'Species', min = 5)
+def df_strat_split(
+    df_input, stratify_colname = 'Species', 
+    frac_train = 0.6, frac_val = 0.2, frac_test = 0.2, 
+    random_state = None):
